230548c539666a3cfaede7ec0a5c5c858453d5b8
Tommy Karlsson <pctkar@gmail.com>
2019-09-17 11:00:45 +0200

    Implementerade att varje patron får en egen task. Det finns nu en kö till öl som både bartender och Patron använder sig av.

--- Lab6_Pub/Bartender.cs
+++ Lab6_Pub/Bartender.cs
@@ -1,30 +1,46 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace Lab6_Pub
 {
     class Bartender
     {
-        public Bartender()
+        private const int POUR_BEER_TIME = 3;
+        private const int REST_TIME = 1;
+        private int currentGlasses;
+        public Bartender(int maxGlasses)
         {
-
+            currentGlasses = maxGlasses;
         }
 
-        public void PickUpGlass()
-        {
-            // Vänta på glas om inget finns
+        public int PourBeerTime => POUR_BEER_TIME;
 
-            // 3 sec att plocka upp glas
+        public int RestTime => REST_TIME;
+
+        public int CurrentGlasses { get => currentGlasses; set => currentGlasses = value; }
+
+        public string PourBeer(Patron patron)
+        {
+            return $"Poured a beer for {patron.Name}";
         }
-
-        public void PourBeer()
+        public bool TakeGlass()
         {
-
+            if (currentGlasses > 0)
+            {
+                currentGlasses -= 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
-
-
 }
